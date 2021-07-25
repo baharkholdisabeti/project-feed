d333d8641dd9a066e9631311563085b6e2f90679
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2021-07-24 23:58:32 -0400

    embed landing page css

--- project-feed/src/Pages/Landing.jsx
+++ project-feed/src/Pages/Landing.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import './Landing.css'
+import background from '../assets/vegetables.png'
+import { makeStyles } from '@material-ui/core/styles';
 import LocationListings from '../Components/LocationListings'
 
+const useStyles = makeStyles(() => ({
+  Landing: {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    width: "100%",
+    height: "100%",
+  },
+  center: {
+    margin: "0",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    msTransform: "translate(-50%, -50%)",
+    transform: "translate(-50%, -50%)",
+  }
+}));
+
 const Landing = () => {
+  const classes = useStyles();
+
   return (
-  <div className="Landing">
-    <div className="center">
+  <div className={classes.Landing}>
+    <div className={classes.center}>
       <h1>RBC Impact Project</h1>
       <a class="btn btn-ghost" href="#">Volunteer</a>
       <a class="btn btn-ghost" href="#">Organization</a>
