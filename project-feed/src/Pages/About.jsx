aa4de32f5cb380ff58ec0fe5a59839370f4c4c9b
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2021-07-25 22:33:58 -0400

    about page styling

--- project-feed/src/Pages/About.jsx
+++ project-feed/src/Pages/About.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  sectionProgram: {
+    backgroundColor:  'rgba(204, 231, 209, 0.885)',
+    backgroundSize: 'cover',
+    padding: '80px 0',
+    height: 'auto',
+    width: 'auto',
+  },
+  longCopy: {
+    lineHeight: '145%', 
+    width: '70%',
+    marginLeft: '15%',
+  },
+  fullsize: {
+    width: '100%',
+    height: '100%',
+  },
+  row: {
+    maxWidth: '1140px',
+    margin: '0 auto',
+  },
+}));
 
 const About = () => {
+  const classes = useStyles();
   return (
-    <section className="section-program">
-      <div className="row">
-        <h2>About RBC Impact Project</h2>
-        <p className="long-copy"></p>
-      </div>
+    <div className={classes.fullsize}>
+      <section className={classes.sectionProgram}>
+        <div className={classes.row}>
+          <h2>About RBC Impact Project</h2>
+          <p className={classes.longCopy}></p>
+        </div>
 
-      <div className="row">
-        <p className='about'>We are a group of interns at RBC working on this initiative to make a change</p>
-      </div>
-
-    </section>
+        <div className={classes.row}>
+        <p>Project Feed has been created by a group of interns at RBC under the Believe Impact Program hosted by Sarah Wells, a Candian Olympian Athelete. Through this intitative, we want to make a difference in this world. 
+          Food Insecurity has been a long running problem in the world today. We believe that everyone has the right to fresh nice meal every day, no matter their circumstances. In today's time. fast food resturances and business dispose of thousands of unsold, perishable food. Our goal at Project Feed is to partner with resturants to help distribute this food to those in need instead of having it go to waste. Even one mean given away can make a huge difference. Slowley Project Feed can really help make sure there are no hungry stomachs left.
+        </p>
+        </div>
+      </section>
+    </div>
   );
 }
 
