063377aafae276937b081765de933138fa45db51
Parisha Nizam <parisha.nizam@gmail.com>
2021-08-11 16:47:44 -0400

    Styling pages

--- project-feed/src/Pages/About.jsx
+++ project-feed/src/Pages/About.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import background from '../assets/background.png'
 import { makeStyles } from '@material-ui/core/styles';
+import Card from 'react-bootstrap/Card';
 
 const useStyles = makeStyles(() => ({
   sectionProgram: {
-    backgroundColor:  'rgba(204, 231, 209, 0.885)',
-    backgroundSize: 'cover',
-    padding: '80px 0',
-    height: 'auto',
-    width: 'auto',
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundAttachment: 'fixed',
+    padding: '200px 0',
+    height: '100%',
+    width: '100%',
   },
   longCopy: {
     lineHeight: '145%', 
@@ -27,20 +30,18 @@ const useStyles = makeStyles(() => ({
 const About = () => {
   const classes = useStyles();
   return (
-    <div className={classes.fullsize}>
-      <section className={classes.sectionProgram}>
-        <div className={classes.row}>
-          <h2>About RBC Impact Project</h2>
-          <p className={classes.longCopy}></p>
-        </div>
+    <section className={classes.sectionProgram}>
+      <div classname={classes.fullsize}>
+          <h1>About Project Feed</h1>
+          <Card.Text>
+          Project Feed has been created by a group of interns at RBC under the Believe Impact Program hosted by Sarah Wells, a Candian Olympian Athelete. Through this intitative, we want to make a difference in our communities. 
+              Food Insecurity has been a long running problem in the world today. We believe that everyone has the right to a fresh nice meal every day, no matter their circumstances. In today's time. fast food resturances and business dispose of thousands of unsold, perishable food. Our goal at Project Feed is to partner with resturants to help distribute this food to those in need instead of having it go to waste. Even one mean given away can make a huge difference. Slowly Project Feed can really help make sure there are no hungry stomachs left.
+              With the help of our Partners ot provide us with resources, and our amazing Volunteers to distribute the meals, we will be able to improve food security one meal at a time.
 
-        <div className={classes.row}>
-        <p>Project Feed has been created by a group of interns at RBC under the Believe Impact Program hosted by Sarah Wells, a Candian Olympian Athelete. Through this intitative, we want to make a difference in this world. 
-          Food Insecurity has been a long running problem in the world today. We believe that everyone has the right to fresh nice meal every day, no matter their circumstances. In today's time. fast food resturances and business dispose of thousands of unsold, perishable food. Our goal at Project Feed is to partner with resturants to help distribute this food to those in need instead of having it go to waste. Even one mean given away can make a huge difference. Slowley Project Feed can really help make sure there are no hungry stomachs left.
-        </p>
-        </div>
-      </section>
-    </div>
+          </Card.Text>
+          <Card.Footer>ProjectFeedCanada</Card.Footer>
+      </div>
+    </section>
   );
 }
 
